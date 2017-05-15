aca1bddac05edbabb4506740e5f36e9f6ddf59d9
Ismael Haytam <anonismaa@gmail.com>
2017-05-15 23:12:31 +0200

    Mensajes de orden superior en las colecciones

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,59 @@
 |
 */
 
+use App\Course;
+
+$courses = collect([
+        new Course(['title' => 'OOP', 'premium' => true]),
+        new Course(['title' => 'Primeros pasos con Laravel', 'premium' => false]),
+        new Course(['title' => 'Git', 'premium' => true]),
+        new Course(['title' => 'Laravel 5.4', 'premium' => false]),
+]);
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('courses', function () use ($courses) {
+
+/*
+    $premium = $courses->filter(function ($course) {
+        return $course->premium;
+    });
+
+    $free = $courses->reject(function ($course) {
+        return $course->premium;
+    });
+*/
+
+/*
+    $premium = $courses->filter->premium;
+    $free = $courses->reject->premium;
+
+*/
+
+/*
+    $courses->map->archive();
+*/
+
+/*
+    $premium = $courses->filter->isPremium();
+    $free = $courses->reject->isPremium();
+*/
+
+/*
+    list($premium, $free) = $courses->partition(function ($course) {
+        return $course->premium;
+    });
+*/
+
+/*
+    list($premium, $free) = $courses->partition->premium;
+*/
+
+    list($premium, $free) = $courses->partition->isPremium();
+
+
+    dd($courses->toArray(), $premium->toArray(), $free->toArray());
+
 });
